cc29362e0a8e64e58f37e0474af44e64df8aa701
DDBalazs <csokebalazszsolt99@gmail.com>
2025-02-24 13:29:52 +0100

    2025.02.24.

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -1,6 +1,38 @@
 @extends('layout')
 @section('content')
+{{-- https://www.youtube.com/watch?v=d-VIORM1X2o --}}
 
-
+    <main class="reg">
+        <div class="container sign">
+            <div class="form-box">
+                <form action="">
+                    <h2>Belépés</h2>
+                    <div class="input-box">
+                        <input type="text" required>
+                        <label for="">Név</label>
+                    </div>
+                    <div class="input-box">
+                        <input type="text" required>
+                        <label for="">E-Mail</label>
+                    </div>
+                    <div class="input-box">
+                        <input type="text" required>
+                        <label for="">Jelszó</label>
+                    </div>
+                    <div class="forget-section">
+                        <p>
+                            <input type="checkbox" name="" id="">
+                            Remember me
+                        </p>
+                        <a href="">Elfelejtette a jelszavát?</a>
+                    </div>
+                    <button class="btn">Bejelentkezés</button>
+                    <div class="account-creation">
+                        <span>Nincs még fiókod?<a href="">Regisztrálj</a></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
 
 @endsection
