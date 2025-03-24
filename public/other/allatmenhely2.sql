c25399bbcd0c3facc871d9636eb1432fc56dada0
DDBalazs <csokebalazszsolt99@gmail.com>
2025-03-24 11:28:15 +0100

    2025.03.24.
    database fixing, insert

--- public/other/allatmenhely2.sql
+++ public/other/allatmenhely2.sql
@@ -1,10 +1,23 @@
+CREATE TABLE `meret` (
+  `meret_id` INT PRIMARY KEY NOT NULL,
+  `kategoria` VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE `fajta` (
+  `fajta_id` INT PRIMARY KEY NOT NULL,
+  `faj` VARCHAR(30) NOT NULL,
+  `pontos_fajta` VARCHAR(30) NOT NULL
+);
+
 CREATE TABLE `allat` (
   `allat_id` INT PRIMARY KEY AUTO_INCREMENT,
   `nev` VARCHAR(30) NOT NULL,
   `fajta_id` INT NOT NULL,
+  FOREIGN KEY (`fajta_id`) REFERENCES `fajta` (`fajta_id`),
   `chip_sorszam` VARCHAR(60) UNIQUE,
   `szuldatum` DATE NOT NULL,
   `meret_id` INT NOT NULL,
+  FOREIGN KEY (`meret_id`) REFERENCES `meret` (`meret_id`),
   `szin` VARCHAR(30) NOT NULL,
   `nem` BOOLEAN NOT NULL,
   `ivartalanitott` BOOLEAN NOT NULL,
@@ -12,22 +25,11 @@ CREATE TABLE `allat` (
   `megjegyzes` TEXT
 );
 
-CREATE TABLE `meret` (
-  `meret_id` INT PRIMARY KEY NOT NULL,
-  `kategoria` VARCHAR(30) NOT NULL
-);
-
 CREATE TABLE `oltas` (
   `chip_sorszam` VARCHAR(60),
+  FOREIGN KEY (`chip_sorszam`) REFERENCES `allat` (`chip_sorszam`),
   `oltas_tipusa` VARCHAR(30) NOT NULL,
   `datum` DATE NOT NULL
-);
-
-CREATE TABLE `foglalt` (
-  `allat_id` INT NOT NULL,
-  `datum` DATE NOT NULL,
-  `onkentes_id` INT,
-  `teljesitve` BOOLEAN
 );
 
 CREATE TABLE `onkentes` (
@@ -38,10 +40,13 @@ CREATE TABLE `onkentes` (
   `tel` VARCHAR(30)
 );
 
-CREATE TABLE `fajta` (
-  `fajta_id` INT PRIMARY KEY NOT NULL,
-  `faj` VARCHAR(30) NOT NULL,
-  `pontos_fajta` VARCHAR(30) NOT NULL
+CREATE TABLE `foglalt` (
+  `allat_id` INT NOT NULL,
+  FOREIGN KEY (`allat_id`) REFERENCES `allat` (`allat_id`),
+  `datum` DATE NOT NULL,
+  `onkentes_id` INT,
+  FOREIGN KEY (`onkentes_id`) REFERENCES `onkentes` (`onkentes_id`),
+  `teljesitve` BOOLEAN
 );
 
 CREATE TABLE `alkalmazott` (
@@ -53,32 +58,8 @@ CREATE TABLE `alkalmazott` (
 
 CREATE TABLE `alkalmazott_user` (
   `alkalmazott_id` INT PRIMARY KEY,
+  FOREIGN KEY (`alkalmazott_id`) REFERENCES `alkalmazott` (`alkalmazott_id`),
   `felhasznalo` VARCHAR(30) NOT NULL,
   `password` TEXT NOT NULL,
   `beosztas` VARCHAR(12) NOT NULL
 );
-
-
-ALTER TABLE `allat`
-ADD CONSTRAINT `fajta_allat_id_fkey` FOREIGN KEY (`fajta_id`) REFERENCES `fajta` (`fajta_id`)
-ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE `allat`
-ADD CONSTRAINT `allat_meret_id_fkey` FOREIGN KEY (`meret_id`) REFERENCES `meret` (`meret_id`)
-ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE `foglalt`
-ADD CONSTRAINT `allat_foglalt_id_fkey` FOREIGN KEY (`allat_id`) REFERENCES `allat` (`allat_id`)
-ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE `foglalt`
-ADD CONSTRAINT `onkentes_foglalt_id_fkey` FOREIGN KEY (`onkentes_id`) REFERENCES `onkentes` (`onkentes_id`)
-ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE `alkalmazott_user`
-ADD CONSTRAINT `alkalmazott_user_id_fkey` FOREIGN KEY (`alkalmazott_id`) REFERENCES `alkalmazott` (`alkalmazott_id`)
-ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE `oltas`
-ADD CONSTRAINT `allat_oltas_id_fkey` FOREIGN KEY (`chip_sorszam`) REFERENCES `allat` (`chip_sorszam`)
-ON DELETE RESTRICT ON UPDATE CASCADE;
--- resources/views/allatok.blade.php
+++ resources/views/allatok.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div class="row">
                 <div class="col-3">
-                    <form action="/allatok" method="POST" class="form-control border border-dark">
+                    <form action="/allatok" method="POST" class="form-control border border-dark rounded">
                         <div class="py-3">
                             <h3 class="text-center">Állatkereső szűrő</h3>
                             <hr class="w-75 mx-auto border border-dark my-2">
@@ -17,9 +17,9 @@
                         <div class="py-3">
                             <h5>Állatfaj</h5>
                             <input type="checkbox" name="kperm" id="dog"  value="dog" class="form-check-input">
-                            <label for="dog" class="form-check-label">Kutya</label>
+                            <label for="dog" class="form-check-label">Kutya</label><br>
                             <input type="checkbox" name="kperm" id="cat"  value="cat" class="form-check-input">
-                            <label for="cat" class="form-check-label">Macska</label>
+                            <label for="cat" class="form-check-label">Macska</label><br>
                             <input type="checkbox" name="kperm" id="all" value="all" class="form-check-input">
                             <label for="all" class="form-check-label">Mindekettő</label>
                         </div>
@@ -42,31 +42,31 @@
                         <div class="py-3">
                             <h5>Méret</h5>
                             <input type="checkbox" name="size" id="small"  value="small">
-                            <label for="small">Kicsi</label>
+                            <label for="small">Kicsi</label><br>
                             <input type="checkbox" name="size" id="medium"  value="medium">
-                            <label for="medium">Közepes</label>
+                            <label for="medium">Közepes</label><br>
                             <input type="checkbox" name="size" id="large" value="large">
                             <label for="large">Nagy</label>
                         </div>
                         <div class="py-3">
                             <h5>Jellemzők</h5>
                             <input type="checkbox" name="traits" id="cfreind"  value="cfriend">
-                            <label for="cfriend">Gyermekbarát</label>
+                            <label for="cfriend">Gyermekbarát</label><br>
                             <input type="checkbox" name="traits" id="afriend"  value="afriend">
-                            <label for="afriend">Más állatokkal barátságos</label>
+                            <label for="afriend">Más állatokkal barátságos</label><br>
                             <input type="checkbox" name="traits" id="nofriend" value="nofriend">
                             <label for="nofriend">Különleges igényű</label>
                         </div>
                         <div class="py-3">
                             <h5>Örökbefogadási státusz</h5>
                             <input type="radio" name="status" id="adoptable" value="adoptable">
-                            <label for="adoptable">Örökbefogadható</label>
+                            <label for="adoptable">Örökbefogadható</label><br>
                             <input type="radio" name="status" id="foster" value="foster">
                             <label for="foster">Ideiglenes befogadásra vár</label>
                         </div>
                         <div class="py-3">
                             <h5>Rendezés</h5>
-                            <select name="sort" id="sort">
+                            <select name="sort" id="sort" class="form-control">
                                 <option value="newest">Legújabbak előre</option>
                                 <option value="oldest">Legrégebbiek előre</option>
                                 <option value="name-asc">Név szerint (A-Z)</option>
@@ -74,13 +74,13 @@
                             </select>
                         </div>
                         <div class="py-3">
-                            <button type="submit">Szűrés</button>
-                            <button type="reset">Visszaállítás</button>
+                            <button type="submit" class="btn btn-secondary w-100 my-3">Szűrés</button>
+                            <button type="reset" class="btn btn-secondary w-100">Visszaállítás</button>
                         </div>
                     </form>
                 </div>
                 <div class="col-9">
-                    
+
                 </div>
             </div>
         </div>
